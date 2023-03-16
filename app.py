5713728f94cf90698f419995bb73868dbeafafa7
Gaku_Noguchi <noggaku@yahoo.co.jp>
2023-03-16 16:10:26 +0000

    プロンプトの精度を向上させた

--- app.py
+++ app.py
@@ -22,10 +22,15 @@ if uploaded_file is not None:
     # 音声データからテキストを起こす
     transcript = transcribe(uploaded_file)
 
+    # 文字起こしテキストがセグメント化され、各セグメントが要約されたリストになる
+    # segment_and_summarize関数は、大きなテキストを一定の文字数ごとに分割し、それぞれのセグメントを要約した新しいリストを返す役割を果たす
     segmented_summary = segment_and_summarize(transcript.text, SEGMENTATION_MIN_LENGTH)
 
+# segmented_summary内の各要約セグメントに対応するチェックボックスをStreamlitアプリに表示
+# それぞれのチェックボックスがチェックされているかどうかの真偽値（TrueまたはFalse）をselected_segments_idxというリストに格納
 selected_segments_idx = [ st.checkbox(segment) for segment in segmented_summary ]
 
+# ユーザーがチェックした要約セグメントのみを含むリスト
 selected_segments = [ segmented_summary[i] for i, selected in enumerate(selected_segments_idx) if selected ]
 
 #
@@ -41,22 +46,27 @@ def generate_output():
     if len(liner_note) > LINER_NOTE_MAX_LENGTH:
         liner_note = summarize(liner_note)
 
+    # 文章を整える
+    liner_note = proofread_and_rewrite(liner_note)
+    
+    #画像生成
     image_text = note if len(note) < IMAGE_PROMPT_MAX_LENGTH else summarize(note)
 
-    image_url = create_image(image_text, "512x512")
+    image_urls = create_image(image_text, "512x512", num_images=4)
 
     st.write(f'タイトル: {title}')
     st.write(f'ライナーノート:\n{liner_note}')
     st.write(f'画像のテキスト:\n{image_text}')
-    st.image(image_url)
-    st.write(f'画像 URL: {image_url}')
+    for url in image_urls:
+        st.image(url)
+        st.write(f'画像 URL: {url}')
 
     json_data = {
         'transcript': transcript.text,
         'segmented_summary': segmented_summary,
         'title': title,
         'liner_note': liner_note,
-        'image_url': image_url,
+        'image_urls': image_urls,
     }
     st.download_button('出力データをダウンロード', str(json_data), file_name='record_jacket.json', mime='application/json')
 
--- utils.py
+++ utils.py
@@ -34,7 +34,7 @@ def segmentation(long_text: str, min_length: int):
 
 @st.cache_data
 def summarize(text: str):
-  prompt = "次の内容を4文以内でまとめてください"
+  prompt = "次の内容を4文以内で日本語でまとめてください"
   content = f"{prompt}:「{text}」"
   
   completion = openai.ChatCompletion.create(
@@ -57,7 +57,7 @@ def segment_and_summarize(long_text: str, segmentation_min_length: int):
     for segment in segmented_texts:
         segmented_summary.append(summarize(segment))
 
-        sleep(1) # sleep to avoid rate limit
+        sleep(1) # sleep to avoid rate limit 各要約の生成の間に、sleep(1)を使って1秒間待機します。これは、APIのレート制限を避けるための処理
 
     return segmented_summary
 
@@ -78,15 +78,73 @@ def create_title(text: str):
 
   return completion.choices[0].message.content
 
+# Dall-E2へのプロンプトを英語にする
 @st.cache_data
-def create_image(prompt: str, size:str="1024x1024"):
-  """
-  return url to image
-  """
-  image_response = openai.Image.create(
-    prompt=prompt,
-    n=1,
-    size=size
-  )
+def translate_to_english(text: str):
+    prompt = "このテキストを英語に翻訳してください"
+    content = f"{prompt}:「{text}」"
 
-  return image_response['data'][0]['url']
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": content
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+# Dall-E2へのプロンプトをコンパクトにする
+@st.cache_data
+def create_compact_prompt(text: str):
+    prompt = "I would like to ask Dall-E2 to draw a picture inspired by the following text. Please make it a compact prompt text in about 15 sentences."
+    content = f"{prompt}:「{text}」"
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": content
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
+
+# Dall-E2へ雑誌の表紙風の絵をオーダーする
+@st.cache_data
+def create_image(prompt: str, size:str="1024x1024", num_images=4):
+    """
+    return a list of urls to images
+    """
+    translated_prompt = translate_to_english(prompt)
+    modified_prompt = f'Illustration style for magazine covers, "{translated_prompt}"'
+    image_response = openai.Image.create(
+        prompt=modified_prompt,
+        n=num_images,
+        size=size
+    )
+
+    image_urls = [img['url'] for img in image_response['data']]
+    return image_urls
+
+# 
+@st.cache_data
+def proofread_and_rewrite(text: str):
+    prompt = "この文章を文法的に正しく、そして自然に聞こえるように日本語で体言止めや接続詞を適宜使いながら修正してください"
+    content = f"{prompt}:「{text}」"
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": content
+            }
+        ]
+    )
+
+    return completion.choices[0].message.content
